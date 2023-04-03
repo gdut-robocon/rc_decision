67389ebf541c5293df808a5615e65890ff4db960
robox_xx <1399786770@qq.com>
2023-04-03 15:33:39 +0800

    rc——de

--- src/control_loop.cpp
+++ src/control_loop.cpp
@@ -1,14 +1,3 @@
-/*
- * @Author: JIAlonglong
- * @Date: 2023-01-15 20:20:07
- * @LastEditors: JIAlonglong 2495477531@qq.com
- * @LastEditTime: 2023-03-08 16:00:36
- * @FilePath: /rc_ws/src/rc_fsm/rc_decision/src/control_loop.cpp
- * @Description: 
- * 
- * Copyright (c) 2023 by JIAlonglong 2495477531@qq.com, All Rights Reserved. 
- */
-#include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
 #include <behaviortree_cpp_v3/behavior_tree.h>
 #include "behaviortree_cpp_v3/bt_factory.h"
 #include "behaviortree_cpp_v3/loggers/bt_zmq_publisher.h"
@@ -20,8 +9,8 @@
 #endif
 #include <rc_decision/bt_service_node.h>
 #include <rc_decision/bt_action_node.h>
-#include <rc_decision/aurora/movebase_client.h>
-#include <rc_decision/aurora/lasermove_client.h>
+#include <rc_decision/aurora/laser_goal.h>
+#include <rc_decision/blackboard/blackboard.h>
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "bt_tree");
@@ -30,33 +19,32 @@ int main(int argc, char **argv)
     std::string xml_filename;
     //we use factory to register our custom nodes
     BT::BehaviorTreeFactory factory;
-    
-    factory.registerNodeType<rc_decision::MoveBase>("MoveBase");
-    BT::NodeBuilder lasermove_build=[&root_nh,&nh](const std::string& name,const BT::NodeConfiguration& config){
-      return std::make_unique<rc_decision::lasermove>(name,config,root_nh,nh);
-    };
-    factory.registerBuilder<rc_decision::lasermove>("lasermove",lasermove_build);
+
+    factory.registerNodeType<rc_decision::laser_goal>("laser_goal"); 
+    factory.registerNodeType<rc_decision::BlackboardRead>("BlackboardRead");
+
 
     std::string file_path = nh.param("file_path", std::string(" "));
     auto tree = factory.createTreeFromFile(file_path);
     ROS_INFO("Loading XML :%s", file_path.c_str());
     // Create a logger
-    BT::StdCoutLogger logger_cout(tree);
+    // BT::StdCoutLogger logger_cout(tree);
     BT::NodeStatus status = BT::NodeStatus::IDLE;
 
     #ifdef ZMQ_FOUND
     // This logger publish status changes using ZeroMQ. Used by Groot
     PublisherZMQ publisher_zmq(tree);
     #endif
-
+    ros::Rate loop_rate(5); 
     // Keep on ticking until you get either a SUCCESS or FAILURE state
-  while( ros::ok() && (status == BT::NodeStatus::IDLE || status == BT::NodeStatus::RUNNING))
+  while( ros::ok())
   {
-    ros::spinOnce();
     status = tree.tickRoot();
     std::cout << status << std::endl;
-    ros::Duration sleep_time(0.01);
-    sleep_time.sleep();
+    // ros::Duration sleep_time(0.01);
+    // sleep_time.sleep();
+    loop_rate.sleep();
+    ros::spinOnce();
   }
 
   return 0;
---
+++ src/laser_goal.cpp
@@ -0,0 +1,77 @@
+#include <ros/ros.h>
+#include <rc_decision/aurora/laser_goal.h>
+#include <move_base_msgs/MoveBaseAction.h>
+#include <tf/transform_datatypes.h>
+BT::NodeStatus rc_decision::laser_goal::onStart()
+{
+
+  if (!ac.waitForServer(ros::Duration(2.0))) {
+    ROS_ERROR("Can't contact move_base server");
+    return BT::NodeStatus::FAILURE;
+  }
+
+  if ( !getInput<Pose2D>("Goal_point", goal_point))
+  {
+    throw BT::RuntimeError("missing required input [Goal_point]");
+  }
+  ROS_INFO("[ MoveBase: SEND REQUEST ]. goal: x=%f y=%f theta=%f\n",
+         goal_point.x, goal_point.y, goal_point.theta);
+
+
+if(goal_point.x!=0&&goal_point.y!=0&&std::isfinite(goal_point.x)&&std::isfinite(goal_point.y))
+{
+ // Build the message from Pose2D
+  move_base_msgs::MoveBaseGoal msg;
+  msg.target_pose.header.frame_id = "map";
+  msg.target_pose.header.stamp = ros::Time::now();
+  msg.target_pose.pose.position.x = goal_point.x;
+  msg.target_pose.pose.position.y = goal_point.y;
+    // msg.target_pose.pose.position.x = 2;
+    // msg.target_pose.pose.position.y = 1;
+    msg.target_pose.pose.orientation.z = 0;
+    msg.target_pose.pose.orientation.w = 1;
+
+  // We use this counter to simulate an action that takes a certain
+  // amount of time to be completed (200 ms)
+  ac.sendGoal(msg);
+  _aborted = false;
+  while (!_aborted && !ac.waitForResult(ros::Duration(0.02))) {
+    // polling at 50 Hz. No big deal in terms of CPU
+  }
+ if (_aborted) {
+    // this happens only if method halt() was invoked
+    //_client.cancelAllGoals();
+    ROS_ERROR("MoveBase aborted");
+    return BT::NodeStatus::FAILURE;
+  }
+  if (ac.getState() != actionlib::SimpleClientGoalState::SUCCEEDED) {
+    ROS_ERROR("MoveBase failed");
+    return BT::NodeStatus::FAILURE;
+  }
+  return BT::NodeStatus::RUNNING;
+}
+else
+{
+  return BT::NodeStatus::SUCCESS;
+}
+
+  
+}
+
+BT::NodeStatus rc_decision::laser_goal::onRunning()
+{
+  //Judge whether the position of the vehicle has arrived
+  if(0.7<abs(the_best_way.data[0])<0.9 && 0.8<abs(the_best_way.data[1])<0.9)
+  // if(true)
+  {
+    ROS_INFO("Target reached");
+    return BT::NodeStatus::SUCCESS;
+  }
+    
+  return BT::NodeStatus::RUNNING;
+}
+
+void rc_decision::laser_goal::onHalted()
+{
+  printf("[ MoveBase: ABORTED ]");
+}
