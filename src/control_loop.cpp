fb7650e9907f94a847d274125e40eea51e00e713
robox-xx <118261752+robox-xx@users.noreply.github.com>
2023-04-05 15:24:31 +0800

    依据源代码修改

--- src/control_loop.cpp
+++ src/control_loop.cpp
@@ -1,13 +1,3 @@
-/*
- * @Author: JIAlonglong
- * @Date: 2023-01-15 20:20:07
- * @LastEditors: JIAlonglong 2495477531@qq.com
- * @LastEditTime: 2023-03-08 16:00:36
- * @FilePath: /rc_ws/src/rc_fsm/rc_decision/src/control_loop.cpp
- * @Description: 
- * 
- * Copyright (c) 2023 by JIAlonglong 2495477531@qq.com, All Rights Reserved. 
- */
 #include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
 #include <behaviortree_cpp_v3/behavior_tree.h>
 #include "behaviortree_cpp_v3/bt_factory.h"
@@ -22,6 +12,8 @@
 #include <rc_decision/bt_action_node.h>
 #include <rc_decision/aurora/movebase_client.h>
 #include <rc_decision/aurora/lasermove_client.h>
+#include <rc_decision/aurora/laser_goal.h>
+#include <rc_decision/blackboard/blackboard.h>
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "bt_tree");
@@ -37,6 +29,11 @@ int main(int argc, char **argv)
     };
     factory.registerBuilder<rc_decision::lasermove>("lasermove",lasermove_build);
 
+    factory.registerNodeType<rc_decision::laser_goal>("laser_goal"); 
+    factory.registerNodeType<rc_decision::BlackboardRead>("BlackboardRead");
+
+
+
     std::string file_path = nh.param("file_path", std::string(" "));
     auto tree = factory.createTreeFromFile(file_path);
     ROS_INFO("Loading XML :%s", file_path.c_str());
@@ -50,13 +47,13 @@ int main(int argc, char **argv)
     #endif
 
     // Keep on ticking until you get either a SUCCESS or FAILURE state
-  while( ros::ok() && (status == BT::NodeStatus::IDLE || status == BT::NodeStatus::RUNNING))
+    ros::Rate loop_rate(5); 
+  while( ros::ok())
   {
-    ros::spinOnce();
     status = tree.tickRoot();
     std::cout << status << std::endl;
-    ros::Duration sleep_time(0.01);
-    sleep_time.sleep();
+    loop_rate.sleep();
+    ros::spinOnce();
   }
 
   return 0;
