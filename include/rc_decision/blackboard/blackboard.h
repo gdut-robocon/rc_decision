733a50ddc2e72a0b44288e9bbc48b4681b270796
robox-xx <118261752+robox-xx@users.noreply.github.com>
2023-04-05 13:47:13 +0800

    Add BT laser goal (#7)
    * rc_decision
    
    * 本包实现了利用bt树决策去获取数据完成小车向最近的柱子停靠的过程
    
    * 解释一下，laser_goal的那个回调函数是用来判断小车是否到达的，这个是小车坐标系下的雷达电位，不是goal_position的点位
    
    * update ladar_goal.cpp

--- include/rc_decision/blackboard/blackboard.h
+++ include/rc_decision/blackboard/blackboard.h
@@ -1,36 +1,54 @@
 /*
- * @Author: JIAlonglong
- * @Date: 2023-01-17 22:20:38
- * @LastEditors: JIAlonglong 2495477531@qq.com
- * @LastEditTime: 2023-03-06 17:14:57
- * @FilePath: /rc_ws/src/rc_fsm/rc_decision/include/rc_decision/blackboard/blackboard.h
- * @Description: blackboard is a node help others' nodes to communicate with rc_msgs
- * 
- * Copyright (c) 2023 by JIAlonglong 2495477531@qq.com, All Rights Reserved. 
+ * @Author: robox-xx 1399786770@qq.com
+ * @Date: 2023-03-15 11:04:27
+ * @LastEditors: robox-xx 1399786770@qq.com
+ * @LastEditTime: 2023-04-03 14:19:33
+ * @FilePath: /rc_ws/src/rc_decision/include/rc_decision/blackboard/blackboard.h
  */
 #pragma once
-#include <rc_msgs/ActionData.h>
-#include <rc_msgs/ActuatorState.h>
-#include <rc_msgs/ChassisCmd.h>
-#include <rc_msgs/GimbalCmd.h>
-#include <rc_msgs/GimbalDesError.h>
-#include <rc_msgs/LpData.h>
-#include <rc_msgs/IbusData.h>
-#include <rc_msgs/GoalPoint.h>
 #include <rc_decision/bt_action_node.h>
 #include <rc_decision/bt_service_node.h>
+#include <std_msgs/Float64MultiArray.h>
+
 namespace rc_decision
 {
-class BlackboardRead : public BT::AsyncActionNode
+class BlackboardRead : public BT::SyncActionNode
 {
     public:
-    BlackboardRead(const std::string& name, const BT::NodeConfiguration& config, const ros::NodeHandle& root_nh,
-        const ros::NodeHandle& nh)
+    BlackboardRead(const std::string& name, const BT::NodeConfiguration& config): BT::SyncActionNode(name, config)
     {
+        ros::NodeHandle nh;
+        radar_tf_data = nh.subscribe("/map/shootPosition", 5, &BlackboardRead::LadarCallback, this);
+    };
+    
+    //Initialization of keys
+    static BT::PortsList providedPorts() {
+        BT::PortsList ports_list;   
+        ports_list.insert(BT::OutputPort<Pose2D>("Goal"));
+        return ports_list;
+    }
 
+    //Adding data to the blackboard
+    BT::NodeStatus tick() override {
+    
+        setOutput("Goal", goal);
+        ROS_INFO("ii sss xxx:%f, yyy:%f", goal.x,goal.y);
+        
+        return BT::NodeStatus::SUCCESS;
     };
 
-    private:
 
+
+    private:
+    
+    ros::Subscriber radar_tf_data;
+    Pose2D goal;
+
+    void LadarCallback(const std_msgs::Float64MultiArray msg)
+   {
+        goal.x=msg.data[0];
+        goal.y=msg.data[1];  
+        goal.theta = atan(goal.x/goal.y);
+   }
 };
 } //namespace rc_decision
--- src/control_loop.cpp
+++ src/control_loop.cpp
@@ -1,27 +1,17 @@
-/*
- * @Author: JIAlonglong
- * @Date: 2023-01-15 20:20:07
- * @LastEditors: JIAlonglong 2495477531@qq.com
- * @LastEditTime: 2023-03-08 16:00:36
- * @FilePath: /rc_ws/src/rc_fsm/rc_decision/src/control_loop.cpp
- * @Description: 
- * 
- * Copyright (c) 2023 by JIAlonglong 2495477531@qq.com, All Rights Reserved. 
- */
-#include <behaviortree_cpp_v3/loggers/bt_cout_logger.h>
 #include <behaviortree_cpp_v3/behavior_tree.h>
 #include "behaviortree_cpp_v3/bt_factory.h"
 #include "behaviortree_cpp_v3/loggers/bt_zmq_publisher.h"
 #include "behaviortree_cpp_v3/loggers/bt_minitrace_logger.h"
 #include "behaviortree_cpp_v3/loggers/bt_file_logger.h"
+#include "behaviortree_cpp_v3/loggers/bt_cout_logger.h"
 #include <ros/ros.h>
 #ifdef SUPPORT_OPENVINO
   #include "openvino_event.h"
 #endif
 #include <rc_decision/bt_service_node.h>
 #include <rc_decision/bt_action_node.h>
-#include <rc_decision/aurora/movebase_client.h>
-#include <rc_decision/aurora/lasermove_client.h>
+#include <rc_decision/aurora/laser_goal.h>
+#include <rc_decision/blackboard/blackboard.h>
 int main(int argc, char **argv)
 {
     ros::init(argc, argv, "bt_tree");
@@ -30,12 +20,10 @@ int main(int argc, char **argv)
     std::string xml_filename;
     //we use factory to register our custom nodes
     BT::BehaviorTreeFactory factory;
-    
-    factory.registerNodeType<rc_decision::MoveBase>("MoveBase");
-    BT::NodeBuilder lasermove_build=[&root_nh,&nh](const std::string& name,const BT::NodeConfiguration& config){
-      return std::make_unique<rc_decision::lasermove>(name,config,root_nh,nh);
-    };
-    factory.registerBuilder<rc_decision::lasermove>("lasermove",lasermove_build);
+
+    factory.registerNodeType<rc_decision::laser_goal>("laser_goal"); 
+    factory.registerNodeType<rc_decision::BlackboardRead>("BlackboardRead");
+
 
     std::string file_path = nh.param("file_path", std::string(" "));
     auto tree = factory.createTreeFromFile(file_path);
@@ -48,15 +36,14 @@ int main(int argc, char **argv)
     // This logger publish status changes using ZeroMQ. Used by Groot
     PublisherZMQ publisher_zmq(tree);
     #endif
-
+    ros::Rate loop_rate(5); 
     // Keep on ticking until you get either a SUCCESS or FAILURE state
-  while( ros::ok() && (status == BT::NodeStatus::IDLE || status == BT::NodeStatus::RUNNING))
+  while( ros::ok())
   {
-    ros::spinOnce();
     status = tree.tickRoot();
     std::cout << status << std::endl;
-    ros::Duration sleep_time(0.01);
-    sleep_time.sleep();
+    loop_rate.sleep();
+    ros::spinOnce();
   }
 
   return 0;
