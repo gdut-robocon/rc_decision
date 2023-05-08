ab6e67c061a36a1fb79445f1f55efb958d269f4f
SDgg01 <119867370+SDgg01@users.noreply.github.com>
2023-05-08 20:45:25 +0800

    Update ChassisMove.h

--- include/rc_decision/aurora/ChassisMove.h
+++ include/rc_decision/aurora/ChassisMove.h
@@ -22,6 +22,7 @@ public:
   BT::NodeStatus onRunning() override;
 
   void onHalted() override;
+  
 private:
   ros::Publisher cmd_vel_pub_;
   geometry_msgs::Twist cmd_vel_;
@@ -30,4 +31,4 @@ private:
   bool _aborted;
 };
 
-}//namespace rc_decision
+}  // namespace rc_decision
