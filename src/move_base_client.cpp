30e1ce52f9ec68bfa866f1630ac90dff4fd7f8cf
SDgg01 <119867370+SDgg01@users.noreply.github.com>
2023-05-08 20:14:40 +0800

    Update move_base_client.cpp

--- src/move_base_client.cpp
+++ src/move_base_client.cpp
@@ -4,23 +4,26 @@
  * @LastEditors: JIAlonglong 2495477531@qq.com
  * @LastEditTime: 2023-03-08 14:16:36
  * @FilePath: /rc_ws/src/rc_fsm/rc_decision/src/move_base_client.cpp
- * @Description: 
- * 
- * Copyright (c) 2023 by JIAlonglong 2495477531@qq.com, All Rights Reserved. 
+ * @Description:
+ *
+ * Copyright (c) 2023 by JIAlonglong 2495477531@qq.com, All Rights Reserved.
  */
 #include <rc_decision/aurora/movebase_client.h>
 #include <ros/ros.h>
-//you can also write in tick function
-BT::NodeStatus rc_decision::MoveBase::tick() {
+// you can also write in tick function
+BT::NodeStatus rc_decision::MoveBase::tick()
+{
   // if no server is present, fail after 2 seconds
-  if (!_client.waitForServer(ros::Duration(2.0))) {
+  if (!_client.waitForServer(ros::Duration(2.0)))
+  {
     ROS_ERROR("Can't contact move_base server");
     return BT::NodeStatus::FAILURE;
   }
 
   // Take the goal from the InputPort of the Node
   Pose2D goal;
-    if (!getInput<Pose2D>("goal", goal)) {
+  if (!getInput<Pose2D>("goal", goal))
+  {
     // if I can't get this, there is something wrong with your BT.
     // For this reason throw an exception instead of returning FAILURE
     throw BT::RuntimeError("missing required input [goal]");
@@ -41,18 +44,21 @@ BT::NodeStatus rc_decision::MoveBase::tick() {
   tf::quaternionTFToMsg(rot, msg.target_pose.pose.orientation);
 
   _client.sendGoal(msg);
-  while (!_aborted && !_client.waitForResult(ros::Duration(0.02))) {
+  while (!_aborted && !_client.waitForResult(ros::Duration(0.02)))
+  {
     // polling at 50 Hz. No big deal in terms of CPU
   }
 
-  if (_aborted) {
+  if (_aborted) 
+  {
     // this happens only if method halt() was invoked
     //_client.cancelAllGoals();
     ROS_ERROR("MoveBase aborted");
     return BT::NodeStatus::FAILURE;
   }
 
-  if (_client.getState() != actionlib::SimpleClientGoalState::SUCCEEDED) {
+  if (_client.getState() != actionlib::SimpleClientGoalState::SUCCEEDED)
+  {
     ROS_ERROR("MoveBase failed");
     return BT::NodeStatus::FAILURE;
   }
