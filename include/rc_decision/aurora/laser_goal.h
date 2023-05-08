3687ceec6a24b19e1c92870c272dafe32453b846
SDgg01 <119867370+SDgg01@users.noreply.github.com>
2023-05-08 18:58:03 +0800

    Update laser_goal.h

--- include/rc_decision/aurora/laser_goal.h
+++ include/rc_decision/aurora/laser_goal.h
@@ -8,7 +8,7 @@
 
 #pragma once
 #include <math.h>
-#include<chrono>
+#include <chrono>
 #include <iostream>
 #include <ros/ros.h>
 #include <behaviortree_cpp_v3/action_node.h>
@@ -18,46 +18,47 @@
 #include <std_msgs/Float64MultiArray.h>
 #include <rc_decision/aurora/movebase_client.h>
 
-namespace rc_decision {
-
-class laser_goal : public BT::StatefulActionNode {
- public:
-  laser_goal(const std::string& name, const BT::NodeConfiguration& config) 
-      : BT::StatefulActionNode(name, config), 
-      ac("move_base", true)
-    {
-        ros::NodeHandle n;
-        radar_org_data = n.subscribe("/radar/shootPosition", 5, &laser_goal::LaserCallback, this);
-    };
+namespace rc_decision
+{
+class laser_goal : public BT::StatefulActionNode
+{
+public:
+  laser_goal(const std::string& name, const BT::NodeConfiguration& config)
+    : BT::StatefulActionNode(name, config), ac("move_base", true)
+  {
+    ros::NodeHandle n;
+    radar_org_data = n.subscribe("/radar/shootPosition", 5, &laser_goal::LaserCallback, this);
+  };
 
   // Initialization of keys
-  static BT::PortsList providedPorts() {
-    BT::PortsList ports_list;   
+  static BT::PortsList providedPorts()
+  {
+    BT::PortsList ports_list;  
     ports_list.insert(BT::InputPort<Pose2D>("Goal_point"));
     return ports_list;
   }
 
-    BT::NodeStatus onStart() override;
+  BT::NodeStatus onStart() override;
 
-    BT::NodeStatus onRunning() override;
+  BT::NodeStatus onRunning() override;
 
-    void onHalted() override;
+  void onHalted() override;
 
-  private:
-    Pose2D goal_point;
-    typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
-    MoveBaseClient ac;
-    bool _aborted;
-    
-    std_msgs::Float64MultiArray the_best_way;
-    ros::Subscriber radar_org_data;
-    std::chrono::system_clock::time_point _completion_time;
-    void LaserCallback(const std_msgs::Float64MultiArray msg)
-    {
-      the_best_way=msg;
-    };
+private:
+  Pose2D goal_point;
+  typedef actionlib::SimpleActionClient<move_base_msgs::MoveBaseAction> MoveBaseClient;
+  MoveBaseClient ac;
+  bool _aborted;
+
+  std_msgs::Float64MultiArray the_best_way;
+  ros::Subscriber radar_org_data;
+  std::chrono::system_clock::time_point _completion_time;
+  void LaserCallback(const std_msgs::Float64MultiArray msg)
+  {
+    the_best_way = msg;
+  };
     
 };
 
-}
+}  // namespace rc_decision
 // laser_goal
