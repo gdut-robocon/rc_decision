1c3b5ea226644d1709d152169e837e184b067ae6
SDgg01 <119867370+SDgg01@users.noreply.github.com>
2023-05-08 20:51:14 +0800

    Update RosJoy.h

--- include/rc_decision/blackboard/RosJoy.h
+++ include/rc_decision/blackboard/RosJoy.h
@@ -11,33 +11,39 @@ namespace rc_decision
 class RosJoy : public BT::SyncActionNode
 {
 public:
-    RosJoy(const std::string& name, const BT::NodeConfiguration& config):BT::SyncActionNode(name, config){};
+  RosJoy(const std::string& name, const BT::NodeConfiguration& config) : BT::SyncActionNode(name, config){};
 
-    static BT::PortsList providedPorts(){
-        BT::PortsList ports;
-        ports.insert(BT::OutputPort<float>("Joy_LX"));
-        ports.insert(BT::OutputPort<float>("Joy_LY"));
-        ports.insert(BT::OutputPort<float>("Joy_RY"));
-        return ports;
-    }
+  static BT::PortsList providedPorts()
+  {
+    BT::PortsList ports;
+    ports.insert(BT::OutputPort<float>("Joy_LX"));
+    ports.insert(BT::OutputPort<float>("Joy_LY"));
+    ports.insert(BT::OutputPort<float>("Joy_RY"));
+    return ports;
+  }
 
-    BT::NodeStatus tick() override{
-        ros::NodeHandle n("~");
-        ros::NodeHandle controller_nh;
-        joy_sub_=controller_nh.subscribe("joy", 10, &RosJoy::joyCallback, this);
-        n.getParam("axes_lx",linear_x);
-        n.getParam("axes_ly",linear_y);
-        n.getParam("axes_angular",angular_);
-        if (joy_received_){
-            setOutput("Joy_LX",joy_lx_);
-            setOutput("Joy_LY",joy_ly_);
-            setOutput("Joy_RY",joy_ry_);
-            ROS_INFO("Joy_LX: %f, Joy_LY: %f, Joy_RY: %f",joy_lx_,joy_ly_,joy_ry_);
-            return BT::NodeStatus::SUCCESS;
-        } else{
-            return BT::NodeStatus::FAILURE;
-        }
+  BT::NodeStatus tick() override
+  {
+    ros::NodeHandle n("~");
+    ros::NodeHandle controller_nh;
+    joy_sub_ = controller_nh.subscribe("joy", 10, &RosJoy::joyCallback, this);
+    n.getParam("axes_lx", linear_x);
+    n.getParam("axes_ly", linear_y);
+    n.getParam("axes_angular", angular_);
+    if (joy_received_)
+    {
+      setOutput("Joy_LX", joy_lx_);
+      setOutput("Joy_LY", joy_ly_);
+      setOutput("Joy_RY", joy_ry_);
+      ROS_INFO("Joy_LX: %f, Joy_LY: %f, Joy_RY: %f", joy_lx_, joy_ly_, joy_ry_);
+      return BT::NodeStatus::SUCCESS;
+    } 
+    else
+    {
+      return BT::NodeStatus::FAILURE;
     }
+  }
+
 private:
     void joyCallback(const sensor_msgs::Joy::ConstPtr& joy)
     {
