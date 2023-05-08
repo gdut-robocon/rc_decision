d2348b9df81c3e2e1c77691c80408feb4cbd12d3
SDgg01 <119867370+SDgg01@users.noreply.github.com>
2023-05-08 19:59:42 +0800

    Update control_loop.cpp

--- src/control_loop.cpp
+++ src/control_loop.cpp
@@ -3,45 +3,43 @@
 #include <behaviortree_cpp_v3/bt_factory.h>
 #include <ros/ros.h>
 #ifdef SUPPORT_OPENVINO
-  #include "openvino_event.h"
+#include "openvino_event.h"
 #endif
 #include <rc_decision/aurora/movebase_client.h>
 #include <rc_decision/aurora/laser_goal.h>
 #include <rc_decision/aurora/ChassisMove.h>
 #include <rc_decision/blackboard/blackboard.h>
 #include <rc_decision/blackboard/RosJoy.h>
-int main(int argc, char **argv)
+int main(int argc, char** argv)
 {
-    ros::init(argc, argv, "bt_tree");
-    ros::NodeHandle nh("~");
-    ros::NodeHandle root_nh;
-    std::string xml_filename;
-    //we use factory to register our custom nodes
-    BT::BehaviorTreeFactory factory;
-    
-    factory.registerNodeType<rc_decision::MoveBase>("MoveBase");
-    factory.registerNodeType<rc_decision::laser_goal>("laser_goal"); 
-    factory.registerNodeType<rc_decision::BlackboardRead>("BlackboardRead");
-    factory.registerNodeType<rc_decision::RosJoy>("RosJoy");
-    factory.registerNodeType<rc_decision::ChassisMove>("ChassisMove");
+  ros::init(argc, argv, "bt_tree");
+  ros::NodeHandle nh("~");
+  ros::NodeHandle root_nh;
+  std::string xml_filename;
+  // we use factory to register our custom nodes
+  BT::BehaviorTreeFactory factory;
+ 
+  factory.registerNodeType<rc_decision::MoveBase>("MoveBase");
+  factory.registerNodeType<rc_decision::laser_goal>("laser_goal");
+  factory.registerNodeType<rc_decision::BlackboardRead>("BlackboardRead");
+  factory.registerNodeType<rc_decision::RosJoy>("RosJoy");
+  factory.registerNodeType<rc_decision::ChassisMove>("ChassisMove");
+ 
+  std::string file_path = nh.param("file_path", std::string(" "));
+  auto tree = factory.createTreeFromFile(file_path);
+  ROS_INFO("Loading XML :%s", file_path.c_str());
+  // Create a logger
+  BT::StdCoutLogger logger_cout(tree);
+  BT::NodeStatus status = BT::NodeStatus::IDLE;
 
+#ifdef ZMQ_FOUND
+  // This logger publish status changes using ZeroMQ. Used by Groot
+  PublisherZMQ publisher_zmq(tree);
+#endif
 
-
-    std::string file_path = nh.param("file_path", std::string(" "));
-    auto tree = factory.createTreeFromFile(file_path);
-    ROS_INFO("Loading XML :%s", file_path.c_str());
-    // Create a logger
-    BT::StdCoutLogger logger_cout(tree);
-    BT::NodeStatus status = BT::NodeStatus::IDLE;
-
-    #ifdef ZMQ_FOUND
-    // This logger publish status changes using ZeroMQ. Used by Groot
-    PublisherZMQ publisher_zmq(tree);
-    #endif
-
-    // Keep on ticking until you get either a SUCCESS or FAILURE state
-    ros::Rate loop_rate(50);
-  while( ros::ok())
+  // Keep on ticking until you get either a SUCCESS or FAILURE state
+  ros::Rate loop_rate(50);
+  while (ros::ok())
   {
     status = tree.tickRoot();
     std::cout << status << std::endl;
@@ -49,7 +47,5 @@ int main(int argc, char **argv)
     ros::spinOnce();
   }
 
-  return 0;
-
-    
+  return 0;    
 }
