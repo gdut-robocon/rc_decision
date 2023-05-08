327ab0994475586e204043421adda62fad343ac9
SDgg01 <119867370+SDgg01@users.noreply.github.com>
2023-05-08 18:52:31 +0800

    Update ChassisMove.h

--- include/rc_decision/aurora/ChassisMove.h
+++ include/rc_decision/aurora/ChassisMove.h
@@ -11,21 +11,23 @@ namespace rc_decision
 class ChassisMove : public BT::StatefulActionNode
 {
 public:
-    ChassisMove(const std::string& name, const BT::NodeConfiguration& config):BT::StatefulActionNode(name, config){};
-    static BT::PortsList providedPorts(){
-        return {BT::InputPort<float>("Joy_LX"),BT::InputPort<float>("Joy_LY"),BT::InputPort<float>("Joy_RX"),BT::InputPort<float>("Joy_RY")};
-    }
-    BT::NodeStatus onStart() override;
+  ChassisMove(const std::string& name, const BT::NodeConfiguration& config) : BT::StatefulActionNode(name, config){};
+  static BT::PortsList providedPorts()
+  {
+    return { BT::InputPort<float>("Joy_LX"), BT::InputPort<float>("Joy_LY"), BT::InputPort<float>("Joy_RX"),
+             BT::InputPort<float>("Joy_RY") };
+  }
+  BT::NodeStatus onStart() override;
 
-    BT::NodeStatus onRunning() override;
+  BT::NodeStatus onRunning() override;
 
-    void onHalted() override;
+  void onHalted() override;
 private:
-    ros::Publisher cmd_vel_pub_;
-    geometry_msgs::Twist cmd_vel_;
-    float joy_lx_,joy_ly_,joy_rx_,joy_ry_;
-    double linear_x_scale_,linear_y_scale_,angular_z_scale_;
-    bool _aborted;
+  ros::Publisher cmd_vel_pub_;
+  geometry_msgs::Twist cmd_vel_;
+  float joy_lx_, joy_ly_, joy_rx_, joy_ry_;
+  double linear_x_scale_, linear_y_scale_, angular_z_scale_;
+  bool _aborted;
 };
 
 }//namespace rc_decision
