a2740c62a16642f41dd413c0a2f3b7e74e6ebf73
SDgg01 <119867370+SDgg01@users.noreply.github.com>
2023-05-08 21:06:27 +0800

    Update laser_goal.h

--- include/rc_decision/aurora/laser_goal.h
+++ include/rc_decision/aurora/laser_goal.h
@@ -33,7 +33,7 @@ public:
   // Initialization of keys
   static BT::PortsList providedPorts()
   {
-    BT::PortsList ports_list; 
+    BT::PortsList ports_list;
     ports_list.insert(BT::InputPort<Pose2D>("Goal_point"));
     return ports_list;
   }
